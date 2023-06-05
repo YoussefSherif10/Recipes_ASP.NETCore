0726a3bb8b1653c75ea52791ab043624fd22a8d7
yousseffekry <youssefsherif@h-eng.helwan.edu.eg>
2023-06-05 11:49:46 +0300

    make new migration

---
+++ recipe/Migrations/20230605084541_ExtraRecipeFields.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace recipe.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtraRecipeFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Method",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegan",
+                table: "Recipes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegetarian",
+                table: "Recipes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "Ingredient",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Ingredient",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVegan",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "IsVegetarian",
+                table: "Recipes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Method",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "Ingredient",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Ingredient",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- recipe/Models/Recipe.cs
+++ recipe/Models/Recipe.cs
@@ -7,6 +7,8 @@ namespace recipe.Models
 		public string? Name { get; set; }
 		public TimeSpan TimeToCock { get; set; }
 		public bool IsDeleted { get; set; }
+		public bool IsVegan { get; set; }
+		public bool IsVegetarian { get; set; }
 		public string? Method { get; set; }
 		public ICollection<Ingredient>? Ingredients { get; set; } // contain list of ingredients
 	}
